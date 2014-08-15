8cb0f90bb6e196f97d86307aa21252e3b5a66d88
Adam Hardy & <pair+ahardy55.alanflorendo@devbootcamp.com>
2014-08-15 13:55:20 -0400

    add rspec for exercise and workout validations

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,4 +1,7 @@
 class Exercise < ActiveRecord::Base
+  validates :name, presence: true
+  validates :type, inclusion: { in: ["weights", "long distance running", "sprints"], message: "%{value} is not a valid type"}
+
   belongs_to :user
   has_many :exercise_histories
 
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,6 @@
 class Workout < ActiveRecord::Base
+  validates :name, presence: true
+
   belongs_to :user
   has_many :exercises
   has_many :workout_histories
