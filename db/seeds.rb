eefdf8801a8a6ef9e9c6997f77d3a895a0a3184e
alanflorendo <alan.florendo@gmail.com>
2014-08-16 16:10:31 -0400

    add expanded seed file with inter-related data for all models

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,18 @@ require 'csv'
 
 
 # Seed Users
-User.create(email: "immichaelweiss@gmail.com", password: "iamstrong")
-User.create(email: "anthony.edwardsjr@gmail.com", password: "iamstrong")
-User.create(email: "ahardy55@gmail.com", password: "iamstrong")
-User.create(email: "dykrause@gmail.com", password: "iamstrong")
-User.create(email: "alan.florendo@gmail.com", password: "iamstrong")
-
+u1 = User.create(email: "immichaelweiss@gmail.com", password: "iamstrong")
+u2 = User.create(email: "anthony.edwardsjr@gmail.com", password: "iamstrong")
+u3 = User.create(email: "ahardy55@gmail.com", password: "iamstrong")
+u4 = User.create(email: "dykrause@gmail.com", password: "iamstrong")
+u5 = User.create(email: "alan.florendo@gmail.com", password: "iamstrong")
+alans_users = [u1, u2, u3, u4, u5]
 
 # Seed Workouts
 days = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
 adjs = %w(Killer Crazy Heartpounder Core Weights Awesome Midlevel)
-User.all.each do |user|
-  rand(4..8).times.do     # Each user creates 4-8 workouts with inspring names
+alans_users.each do |user|
+  rand(4..8).times do     # Each user creates 4-8 workouts with inspring names
     day = days[rand(days.length-1)]
     adj = adjs[rand(adjs.length-1)]
     workout_name = "#{day} #{adj} Workout"
@@ -38,14 +38,13 @@ end
   	name_weight_exercises << row
   end
 
-
   # For each workout, load 5-15 exercises
   Workout.all.each do |workout|
     rand(5..15).times do
       exercise_type_chooser = rand(1..10) #80% chance weights, 10% LD running, 10% sprints
       if exercise_type_chooser <= 8 #weights
         ex_num = rand(name_weight_exercises.length-1)
-        ex_name = exercise_name[ex_num].split.map(&:capitalize).join(' ')
+        ex_name = name_weight_exercises[ex_num][0]
         tempo_string = "#{rand(0..9)}#{rand(0..9)}#{rand(0..9)}#{rand(0..9)}"
         Exercise.create(workout_type: 'weights', workout_id: workout.id, name: ex_name, weight: rand(25..250), reps: rand(2..16), rest_time: rand(60), tempo: tempo_string)
       elsif exercise_type_chooser === 9 #long distance running
@@ -64,7 +63,7 @@ end
   # Each workout is done by its user 3-10 times
   rand(3..10).times do
     Workout.all.each do |workout|
-      WorkoutHistory.create(user_id: workout.user.id, workout_id: workout.id)
+      WorkoutHistory.create(user_id: workout.user_id, workout_id: workout.id)
     end
   end
 
@@ -72,17 +71,18 @@ end
   # (only one fill-in per WH history--no need to do it multiple times)
   WorkoutHistory.all.each do |wh|
     wh.workout.exercises.each do |ex| # for every exercise in the WH
-      if ex.exercise == "weights"
+      if ex.workout_type == "weights"
         chance_did_exact = rand(1..10)
         if chance_did_exact <= 6 # 60% chance they did it perfectly
           ExerciseHistory.create(workout_history_id: wh.id, exercise_id: ex.id, weight: ex.weight, reps: ex.reps, rest_time: ex.rest_time)
         elsif chance_did_exact <= 9 # 30% chance they did it with variation
           actual_weight = (ex.weight * (rand(50..200)/100) ).round
           actual_reps = (ex.reps * (rand(50..200)/100) ).round
-          actual_rest_time = (ex.rest_time * (rand(50..200)/100) ).round
+          rest_time_as_integer = ex.rest_time.to_i
+          actual_rest_time = (rest_time_as_integer * (rand(50..200)/100) ).round
           ExerciseHistory.create(workout_history_id: wh.id, exercise_id: ex.id, weight: actual_weight, reps: actual_reps, rest_time: actual_rest_time)
         end # 10% chance they didn't do it (don't create an exercise history)
-      elsif ex.exercise == "long distance running"
+      elsif ex.workout_type == "long distance running"
         actual_distance = (ex.distance * (rand(80..140)/100) ).round
         actual_running_time = actual_distance * rand(7..10)
         ExerciseHistory.create(workout_history_id: wh.id, exercise_id: ex.id, distance: actual_distance, running_time: actual_running_time)
