f93850d2a665a51abacedfdfcbe1ff6cf9027ee3
Anthony Edwards Jr <credential.helper>
2014-08-19 12:20:36 -0400

    finished adding js module to append heatmap images to dom

--- app/assets/javascripts/heat_map.js
+++ app/assets/javascripts/heat_map.js
@@ -2,6 +2,13 @@ document.addEventListener('DOMContentLoaded', function(){
 	console.log("loaded")
 
 	svgBodyElements.abs()
+	svgBodyElements.chest()
+	svgBodyElements.leftBisep()
+	svgBodyElements.rightBisep()
+	svgBodyElements.shoulders()
+	svgBodyElements.thighs()
+
+
 
 });
 
@@ -10,12 +17,45 @@ var svgBodyElements = (function (){
 	return{
 	  abs: function(){
 			var svg = document.querySelector(".svg-container")
-			console.log(svg)
 			var img = document.createElement("img")
-			img.setAttribute("src", "abs.svg")
+			img.setAttribute("src", "/assets/abs.svg")
 			img.id="abs";
 			svg.appendChild(img)
-			console.log(img)
+	  },
+	   chest: function(){
+			var svg = document.querySelector(".svg-container")
+			var img = document.createElement("img")
+			img.setAttribute("src", "/assets/chest.svg")
+			img.id="chest";
+			svg.appendChild(img)
+	  },
+	  leftBisep: function(){
+			var svg = document.querySelector(".svg-container")
+			var img = document.createElement("img")
+			img.setAttribute("src", "/assets/leftBisep.svg")
+			img.id="leftBisep";
+			svg.appendChild(img)
+	  },
+	   rightBisep: function(){
+			var svg = document.querySelector(".svg-container")
+			var img = document.createElement("img")
+			img.setAttribute("src", "/assets/rightBisep.svg")
+			img.id="rightBisep";
+			svg.appendChild(img)
+	  },
+	   shoulders: function(){
+			var svg = document.querySelector(".svg-container")
+			var img = document.createElement("img")
+			img.setAttribute("src", "/assets/shoulders.svg")
+			img.id="shoulders";
+			svg.appendChild(img)
+	  },
+	   thighs: function(){
+			var svg = document.querySelector(".svg-container")
+			var img = document.createElement("img")
+			img.setAttribute("src", "/assets/thighs.svg")
+			img.id="thighs";
+			svg.appendChild(img)
 	  }
   };
 })();
