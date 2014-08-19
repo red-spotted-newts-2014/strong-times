e702d49471b8dc2ef45bbdcc157dd63279727d0f
user1011 <immichaelweiss@gmail.com>
2014-08-19 17:59:52 -0400

    Add dynamic heat map opacity based on number of sets completed Add show method to exercise_history controller

--- app/assets/javascripts/heat_map.js
+++ app/assets/javascripts/heat_map.js
@@ -2,18 +2,24 @@ document.addEventListener('DOMContentLoaded', function(){
 	console.log("loaded")
 
 	svgBodyElements.abs()
+	var numSets = $(".num_sets").data("sets")
+	$( "#abs" ).css("opacity", numSets/(numSets + 1));
 
 });
 
 
 var svgBodyElements = (function (){
+
 	return{
 	  abs: function(){
 			var svg = document.querySelector(".svg-container")
 			console.log(svg)
 			var img = document.createElement("img")
-			img.setAttribute("src", "abs.svg")
+
+			img.setAttribute("src", "/assets/abs.svg")
 			img.id="abs";
+
+
 			svg.appendChild(img)
 			console.log(img)
 	  }
