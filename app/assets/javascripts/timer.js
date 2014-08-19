f868e4697f3d964cdc8e1d7ca496d7eec4dc16fc
& <pair+krauzer.krauzer@devbootcamp.com>
2014-08-19 15:18:33 -0400

    get timer to link to and countdown on html

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -3,12 +3,20 @@ $(document).ready(function() {
 
   $(".timer_button").on('click', function(event) {
     event.preventDefault();
-
     time = $(".timer_input").val()
 
-    var timeFunc = function(){timerBody(time)}
+    var timeFunc = function(){
+    var min = Math.floor(time/60);
+    var secs = Math.floor((time-(60*min))/10);
+    var sec = time%10;
+    $(".minute").text(min);
+    $(".seconds").text(secs);
+    $(".second").val("").text(sec);
+      timerBody(time)
+    }
     var timer = setInterval( timeFunc, 1000)
     function timerBody(seconds) {
+      console.log(seconds)
       if (seconds === 0) {
         clearInterval(timer)
       }
