c3bac47186846478b99590219d6823117334f356
& <pair+krauzer.krauzer@devbootcamp.com>
2014-08-20 02:47:44 -0400

    working colorizer function linked to timer

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -3,12 +3,14 @@ $(document).ready(function() {
 
   $(".timer_button").on('click', function(event) {
     event.preventDefault();
-    colorUpDown($(".modal-box"),1000,50,"r")
 
     time = $(".rest_time").data("rest")
     $('#toggler').attr('checked', true)
-    console.log(time)
-
+    // console.log(time)
+    incrementSums = timerColor(time, 190, 70, 127)
+    colorUpDown($(".modal-box"), incrementSums[0], 290, "r")
+    colorUpDown($(".modal-box"), incrementSums[0], 70, "g")
+    colorUpDown($(".modal-box"), incrementSums[0], 127, "b")
     var timeFunc = function(){
     var min = Math.floor(time/60);
     var secs = Math.floor((time-(60*min))/10);
@@ -22,7 +24,7 @@ $(document).ready(function() {
     var timer = setInterval( timeFunc, 1000);
 
     function timerBody(seconds) {
-      console.log(seconds)
+      // console.log(seconds)
       if (seconds === 0) {
         clearInterval(timer)
         $('#toggler').attr('checked', false)
@@ -34,33 +36,33 @@ $(document).ready(function() {
 });
 $(".modal")
 
-function timerColor(seconds, red, blue, green, obj) {
+function timerColor(seconds, red, green, blue) {
   var value = seconds*1000
   var red_inc = Math.floor(value/red)
   var blue_inc = Math.floor(value/blue)
   var green_inc = Math.floor(value/green)
-  return [red_inc, blue_inc, green_inc]
+  return [red_inc, green_inc, blue_inc]
 }
 
 function colorUpDown(object, everyMilSecs, totalInc, color) {
   console.log(totalInc)
   totalInc--
   if (totalInc <= 0) {return}
-  console.log(object.getHexBackgroundColor())
+  // console.log(object.getRGBBackgroundColor())
   if (color === "r"){
-    var red = object.getHexBackgroundColor()[0]+1
-    var green = object.getHexBackgroundColor()[1]
-    var blue = object.getHexBackgroundColor()[2]
+    var red = object.getRGBBackgroundColor()[0]+1
+    var green = object.getRGBBackgroundColor()[1]
+    var blue = object.getRGBBackgroundColor()[2]
   }
   else if (color === "g"){
-    var red = object.getHexBackgroundColor()[0]
-    var green = object.getHexBackgroundColor()[1]-1
-    var blue = object.getHexBackgroundColor()[2]
+    var red = object.getRGBBackgroundColor()[0]
+    var green = object.getRGBBackgroundColor()[1]-1
+    var blue = object.getRGBBackgroundColor()[2]
   }
   else if (color === "b"){
-    var red = object.getHexBackgroundColor()[0]
-    var green = object.getHexBackgroundColor()[1]
-    var blue = object.getHexBackgroundColor()[2]-1
+    var red = object.getRGBBackgroundColor()[0]
+    var green = object.getRGBBackgroundColor()[1]
+    var blue = object.getRGBBackgroundColor()[2]-1
   }
   object.css("background-color","rgb("+red+","+green+","+blue+")")
   setTimeout(function(){colorUpDown(object,everyMilSecs,totalInc, color)}, everyMilSecs)
@@ -69,7 +71,7 @@ function colorUpDown(object, everyMilSecs, totalInc, color) {
 
 $(".modal-box").css("background-color","rgb(15,162,219)")
 
-$.fn.getHexBackgroundColor = function() {
+$.fn.getRGBBackgroundColor = function() {
   var rgb = $(this).css('background-color');
   var hex_rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
   return hex_rgb.slice(1,4).map(function(x){return parseInt(x)})
