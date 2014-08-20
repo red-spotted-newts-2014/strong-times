48d44632b8ae4648e3af9699409f6cc18c939025
user1011 <immichaelweiss@gmail.com>
2014-08-19 21:21:48 -0400

    mergining

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
 	def inline_svg(path)
-	    File.open("app/assets/images/#{path}", "rb") do |file|
+	    File.open("app/assets/images/#{path}") do |file|
 	    raw file.read
 	  end
 	end
